d632246615ccb3c2d0e7314aec9e915b46e86fc5
Greg Miller <gregory.miller.@nike.com>
2023-09-27 01:04:50 -0400

    Does first pass breaking out what appear to be sections defined in the file.
    I'm assuming the four-uppercase-letter sections are some kind
    of header for the next block of binary data.
    
    For now, I'll just collect the FOUR letters, and the data that follows
    into groupds called _sectionds_.
    
    This commit also prints out the "headers" and corresponding numbers.

---
+++ VVRHeaders.js
@@ -0,0 +1,59 @@
+/*
+	VVRHeaders.js
+	-------------
+
+	Exports all the four-letter section headers
+	I found in the various VVR files.
+
+	No idea what they all mean, yet.
+*/
+
+// all the headers I've identified, so far
+const vvrHeaders = [
+	"BMAP",
+	"COLR",
+	"CONN",
+	"CPRF",
+	"DATA",
+	"ELGT",
+	"ESLC",
+	"FEAT",
+	"FORM",
+	"LAYR",
+	"LGHT",
+	"LNUM",
+	"LOCK",
+	"NAME",
+	"PDEF",
+	"PEDT",
+	"PGRP",
+	"PLGR",
+	"PMOD",
+	"PNAV",
+	"POLY",
+	"POSN",
+	"PREF",
+	"PRND",
+	"PRSM",
+	"PUNT",
+	"PWIN",
+	"ROOT",
+	"SLIC",
+	"SURF",
+	"SUTX",
+	"TATR",
+	"TRNS",
+	"TXID",
+	"TXOD",
+	"TXTB",
+	"UNIT",
+	"VERS",
+	"VGER",
+	"VGRS",
+	"VMDL",
+	"VPRF",
+	"VRIF",
+];
+
+// export the array
+module.exports = vvrHeaders;
--- d3dl.js
+++ d3dl.js
@@ -1,2 +1,81 @@
-console.log('hello world');
+/*
+	d3dl.js
+	-------
 
+	DesignIt-3D Loader.
+*/
+
+// load list of headers for parsing the file
+const vvrHeaders = require('./VVRHeaders.js');
+
+// use file system so we can load the binary file
+const fs = require('fs');
+
+// get the file to load,
+// for now we'll also always default to "JUSTCUBE.VVR" for debugging
+const fileToLoad = (process.argv[2] || 'JUSTCUBE.VVR');
+
+// load the file's binary
+const fileBuffer = fs.readFileSync(`./${fileToLoad}`);
+
+// to start, we'll loop over the entire buffer, looking for combinations of four uppercase letters
+// that match one of our vvrHeaders.
+// then, we'll continue recording binary until we hit another header or EOF
+
+// array of VVR sections we'll populate
+const sections = [];
+
+// buffer of last four while we look for our keywords
+let lastFour = [];
+
+// current section data we're gathering, null till we find our first section
+let currentSection = null;
+
+// loop over buffer data
+for (x of fileBuffer) {
+
+	// if we have a current object, push the data to it's data section
+	if(currentSection!=null)
+		currentSection.data.push(x);
+		
+	// get character from buffer byte
+	const c = String.fromCharCode(x);
+
+	// add to our last four & unshift the front if we're over 4 total
+	lastFour.push(c);
+	while(lastFour.length>4)
+		lastFour.shift();
+
+	// get the last-four as a single 4-digit string.
+	const lastFourStr = lastFour.join('');
+
+	// check if the last four match one of our headers & start a new section if we did
+	if(vvrHeaders.includes(lastFourStr)){
+
+		// first, we if have an existing object, we should remove the last four from it's data section
+		if(currentSection!=null)
+			for(let i=0; i<4; i++)
+				currentSection.data.pop();
+
+		// make a new section object using our last four as it's header type
+		const newSection = {
+			header: lastFourStr,
+			data: [],
+		};
+		sections.push(newSection);
+
+		// set as our new current section
+		currentSection = newSection;
+	}
+
+}// next x
+  
+// for now, let's just loop over and print the data we found in a logical way
+for(section of sections){
+	
+	console.log("---------------------------------------");
+	console.log(section.header);
+	if(section.data.length>0)
+		console.log(section.data.join(', '));
+
+}// next section
